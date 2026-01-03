ccd806e8172228ea84ad4ae93e99f74bc18d23ba
Quentin Veys <qveys@uliege.be>
2026-01-03 11:29:22 +0100

    💤 Add delay utility function for async waits

--- utils/formatters.js
+++ utils/formatters.js
@@ -28,3 +28,12 @@ function formatDuration(ms) {
   }
   return `${seconds}s`;
 }
+
+/**
+ * Delay execution for a specified number of milliseconds
+ * @param {number} ms - Milliseconds to delay
+ * @returns {Promise<void>}
+ */
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
