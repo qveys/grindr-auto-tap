bf1267814714255e21db68c07d6decdb78ba68d4
Quentin Veys <qveys@uliege.be>
2026-01-03 11:22:39 +0100

    ⚙️ Add constants.js with DELAYS configuration object

---
+++ utils/constants.js
@@ -0,0 +1,19 @@
+// Constants for the Grindr Auto Tap extension
+// Exported as global object for use in content scripts
+
+window.Constants = {
+  // Delays (in milliseconds)
+  DELAYS: {
+    SHORT: 50,
+    MEDIUM: 100,
+    NORMAL: 200,
+    LONG: 300,
+    VERY_LONG: 500,
+    SECOND: 1000,
+    TWO_SECONDS: 2000,
+    TWO_AND_HALF_SECONDS: 2500,
+    THREE_SECONDS: 3000,
+    RANDOM_MIN: 50,
+    RANDOM_MAX: 50,
+  },
+};
